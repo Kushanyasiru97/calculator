14ef4932eda1300c50b7b56af0e05728da0b7655
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2021-06-24 20:23:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 // ignore: avoid_web_libraries_in_flutter
+import 'package:math_expressions/math_expressions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,10 +18,67 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+
+  String equation="0";
+  String result = "0";
+  String expression ="";
+  double equationFontSize=40.0;
+  double resultFontSize=50.0;
+  buttonPressed(String buttonText){
+    setState(() {
+      if (buttonText == "C"){
+        equation="0";
+        result="0";
+        equationFontSize=40.0;
+        resultFontSize=50.0;
+      }
+      else if(buttonText == "⌫"){
+        equationFontSize=40.0;
+        resultFontSize=50.0;
+        equation=equation.substring(0, equation.length -1);
+        if(equation ==""){
+          equation="0";
+        }
+      }
+
+      else if(buttonText == "⁼"){
+        equationFontSize=40.0;
+        resultFontSize=50.0;
+
+        expression = equation;
+        expression =expression.replaceAll("×", "*");
+        expression =expression.replaceAll("÷", "/");
+        try{
+          Parser p= Parser();
+          Expression exp= p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result= '${exp.evaluate(EvaluationType.REAL,cm)}';
+        }
+        catch(e){
+          result="Error";
+        }
+      }
+      else {
+        equationFontSize=40.0;
+        resultFontSize=50.0;
+        if(equation =="0") {
+          equation = buttonText;
+        }
+        else{
+          equation = equation+buttonText;
+        }
+
+      }
+    }
+    );
+  }
+
   Widget buildButton(String buttonText, double buttonHeight, Color buttonColor) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
       color: buttonColor,
+      // ignore: deprecated_member_use
       child: FlatButton(
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(0.0),
@@ -31,7 +89,7 @@ class _CalculatorState extends State<Calculator> {
             )
         ),
         padding: EdgeInsets.all(18.0),
-        onPressed: null,
+        onPressed: ()  => buttonPressed(buttonText),
         child: Text(
           buttonText,
           style: TextStyle(
@@ -46,11 +104,11 @@ class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan[400],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('CALCULATOR'),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.blue,
         elevation: 15.0,
 
       ),
@@ -60,8 +118,16 @@ class _CalculatorState extends State<Calculator> {
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(15, 25, 25, 0),
             child: Text(
-              "0",
-              style: TextStyle(fontSize: 40,),
+              equation,
+              style: TextStyle(fontSize: equationFontSize),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(15, 35, 15, 0),
+            child: Text(
+              result,
+              style: TextStyle(fontSize: resultFontSize),
             ),
           ),
           Expanded(
@@ -77,8 +143,8 @@ class _CalculatorState extends State<Calculator> {
                     TableRow(
                       children: [
                         buildButton("C", 1.0, Colors.redAccent),
-                        buildButton("X", 1.0, Colors.lightBlue),
-                        buildButton("+", 1.0, Colors.lightBlue),
+                        buildButton("⌫", 1.0, Colors.lightBlue),
+                        buildButton("÷", 1.0, Colors.lightBlue),
 
 
                       ],
@@ -104,20 +170,42 @@ class _CalculatorState extends State<Calculator> {
                         buildButton("9", 1.0, Colors.grey),
                       ],
                     ),
+                    TableRow(
+                      children: [
+                        buildButton("∙", 1.0, Colors.grey),
+                        buildButton("0", 1.0, Colors.grey),
+                        buildButton("00", 1.0, Colors.grey),
+                      ],
+                    )
                   ],
                 ),
               ),
               Container(
-                width: MediaQuery.of(context).size.width *80,
                 child: Table(
                   children: [
                     TableRow(
-                      children: [
-                        buildButton("8", 1.0, Colors.grey),
-                      ]
+                        children: [
+                          buildButton("×", 1.0, Colors.lightBlue),
+                        ]
+                    ),
+                    TableRow(
+                        children: [
+                          buildButton("-", 1.0, Colors.lightBlue),
+                        ]
+                    ),
+                    TableRow(
+                        children: [
+                          buildButton("+", 1.0, Colors.lightBlue),
+                        ]
+                    ),
+                    TableRow(
+                        children: [
+                          buildButton("⁼", 2.0, Colors.green),
+                        ]
                     )
                   ],
                 ),
+                width: MediaQuery.of(context).size.width *.25,
               )
             ],
           )
